6cbee8bd8c27c38f420dfed5773fb0357dad2bfc
1910A <hailey.jaranilla@gmail.com>
2024-10-06 19:24:14 +0800

    Working Basic Backend Functionalities: Register, Login, Retrieve

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const express = require('express');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/user');
+const rateLimiter = require('./middleware/rateLimit');
+const logging = require('./middleware/logging');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    const currentDate = new Date().toISOString();
+    console.log(`[${currentDate}] ${req.method} ${req.originalUrl}`); 
+    next();
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+
+app.use(rateLimiter); 
+app.use(logging);
+app.get('/', (req, res) => {
+    res.json({ message: 'Backend Application' });
+});
+app.use('/api/users', userRoutes);
+
+
+app.use((req, res) => {
+    res.status(404).json({ message: 'Route is not found' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server connected to ${PORT}`);
 });
